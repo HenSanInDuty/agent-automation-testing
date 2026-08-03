f2794b8f9c67088fedcc6475f36fe54acd8ce027
hen <nhantrantrong01@gmail.com>
2026-08-03 21:26:27 +0700

    fix: merge alembic migration heads

---
+++ auto-at-ui/migrations/versions/a3b4c5d6e7f8_merge_activity_and_run_heads.py
@@ -0,0 +1,18 @@
+"""merge activity timeline and run timestamp heads
+
+Revision ID: a3b4c5d6e7f8
+Revises: b3c4d5e6f7a8, f2a3b4c5d6e7
+"""
+
+revision = "a3b4c5d6e7f8"
+down_revision = ("b3c4d5e6f7a8", "f2a3b4c5d6e7")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
